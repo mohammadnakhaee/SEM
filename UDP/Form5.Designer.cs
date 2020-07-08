8fc3d268cd9cc1b17444df8ac1b8f67b4124fd2f
Mohammad Nakhaee <mohammad.nakhaee@uantwerpen.be>
2020-07-08 13:17:13 +0200

    using recovery 4-2020

---
+++ UDP/Form5.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace HelloWorld
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 900;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(144, 490);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(299, 11);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::HelloWorld.Properties.Resources.Connection;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.progressBar1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form5";
+            this.Text = "Form5";
+            this.Load += new System.EventHandler(this.Form5_Load);
+            this.Shown += new System.EventHandler(this.Form5_Shown);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form5_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ UDP/StageForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class StageForm : Form
+    {
+        private int count;
+
+        public StageForm()
+        {
+            InitializeComponent();
+            timer1.Start();
+            this.stage1.valueChanged += new EventHandler(valuechanged);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (count == 0)
+                pictureBox1.BackColor = Color.Red;
+            else
+                pictureBox1.BackColor = Color.Transparent;
+            if (++count > 10) count = 0;
+
+        }
+
+        private void stage1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+        private void valuechanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int x = ((MouseEventArgs)e).X;
+                int y = ((MouseEventArgs)e).Y;
+                // string CompleteOrder = CreateChildCommand("se", "faraday " + value.ToString() + "\r");
+                FormMain.ComPorts[0].Write("us " + x.ToString("000") + "  " + y.ToString("000") + "\r");
+                this.Text = "Stage (" + x.ToString("000") + " " + y.ToString("000") + ")";
+                /*  if (y>0)
+                      FormMain.ComPorts[0].Write("uy+ "+y.ToString()+"\r");
+                 else
+                      FormMain.ComPorts[0].Write("uy- " + (-y).ToString() + "\r");
+                      */
+            }
+            catch
+            { }
+        }
+
+        private void numericUpDown_step_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                FormMain.ComPorts[0].Write("set " + numericUpDown_delay.Value.ToString("00") + " " + numericUpDown_step.Value.ToString("000") + "\r");
+            }
+            catch { }
+        }
+
+        private void numericUpDown_delay_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                FormMain.ComPorts[0].Write("set " + numericUpDown_delay.Value.ToString("00") + " " + numericUpDown_step.Value.ToString("000") + "\r");
+            }
+            catch { }
+        }
+
+        private void StageForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
